68939fb5f1db5048c12238aa2d06077691916a2d
javad <mahdipourjavad1379@gmail.com>
2025-05-28 13:09:40 +0330

    nice

--- src/controller/accessPoint.controller.ts
+++ src/controller/accessPoint.controller.ts
@@ -112,7 +112,7 @@ export class accessPointController{
             isAccess=true
         }
 
-        const obj={id:element.id,englishName:element.englishName,persianName:element.persianName,isAccess}
+        const obj={id:element.id,englishName:element.englishName,persianName:element.persianName,isAccess,children:element.children,parent:element.parent?element.parent:null}
         finalArray.push(obj)
         
       }
